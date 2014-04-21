5dbaee65721b1706cbde6224f07be2000cb61ddd
Gilbert Jabbour <gilbertqwerty1@gmail.com>
2014-04-21 16:38:04 +0300

    Add htcleo files only

--- configdiff.sh
+++ configdiff.sh
@@ -1,2 +1,2 @@
-cat .config |grep -E "^CONFIG_"  | sort  >1 && cat ../dsc-team-dsc-team-kernel-project-hunderteins/.config |grep -E "^CONFIG_"  | sort  >2 && diff -Nruw 2 1 |grep -e "^-CONFIG"  > config-minuses 
+cat .config |grep -E "^CONFIG_"  | sort  >1 && cat config-hunderteins |grep -E "^CONFIG_"  | sort  >2 && diff -Nruw 2 1 |grep -e "^-CONFIG"  > config-minuses 
 rm 1 2 
